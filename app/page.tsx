e87cbdc5cd817be30ed2998edec7b720762afeee
Shaaz <asadakhtar800@gmail.com>
2024-07-02 16:41:04 +0530

    add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <h1>Hello Next JS</h1>
+    <>
+    <h1>Hello Next JS</h1>
+
+    <Button>
+      Click Me
+    </Button>
+    
+    </>
+   
   );
 }
