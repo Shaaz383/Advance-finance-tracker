073728604fd5e28bf37bca7fbb6dd49cb0eb4499
Shaaz <asadakhtar800@gmail.com>
2024-07-04 10:57:32 +0530

    Account Post API

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
-  
+  const { onOpen } = useNewAccount();
 
   return (
     <div>
-      <h1>Home page</h1>
+      <Button onClick={onOpen}>Add an account</Button>
     </div>
   );
 }
